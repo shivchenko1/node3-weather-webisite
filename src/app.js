71595acc553cedf3239ec17804acd9ca8f49889a
shivchenko1 <shivchenko1@gmail.com>
2019-07-16 23:11:25 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const path=require('path')
 const app=express()
+const port=process.env.PORT || 3000
 const hbs=require('hbs')
 const geoCode=require('./utils/geoCode')
 const forecast=require('./utils/forecast')
@@ -77,6 +78,6 @@ app.get('*',(req,res)=>{
         message:'Page not found',
         name:'Shiv Sanket'})
 })
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
 })
